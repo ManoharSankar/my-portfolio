0fe2dcb8a3e4276db75bd1520380af6954b1cd28
ManoharSankar <manoharsankar93@gmail.com>
2025-03-05 10:32:51 +0530

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    const heading = document.getElementById("welcome-message");
+    if (heading) {
+        heading.innerHTML = "Welcome to My Portfolio!";
+    }
+});
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const heading = document.getElementById("welcome-message");
+    if (heading) {
+        heading.innerHTML = "Welcome to My Portfolio!";
+    }
+});
